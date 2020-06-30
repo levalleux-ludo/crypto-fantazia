57feef5cd3f0a26d475a5d6dd005cd045d053262
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-06-30 18:18:02 +0100

    change user on production host

--- deploy-api.bat
+++ deploy-api.bat
@@ -1,5 +1,13 @@
+
+if "%HOST%" equ "" (
+    echo Please enter the host address:
+    set /P HOST=
+)
+
+
 :deploy
-plink -no-antispoof -ssh -i .\env\cryptoFanta-heficed_ubuntu.ppk ubuntu@%HOST% "cd crypto-fantazia && git pull origin master"
+plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_cryptof.ppk" cryptof@%HOST% "[ ! -d "crypto-fantazia" ] && git clone https://github.com/levalleux-ludo/crypto-fantazia"
+plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_cryptof.ppk" cryptof@%HOST% "cd crypto-fantazia && git pull origin master && npm install"
 :Build_Launch
-plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_ubuntu.ppk" ubuntu@%HOST% "killall node"
-plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_ubuntu.ppk" ubuntu@%HOST% "source ./.profile && cd crypto-fantazia/api/ && npm run build && node build/api/src/index.js &"
+plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_cryptof.ppk" cryptof@%HOST% "killall node"
+plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_cryptof.ppk" cryptof@%HOST% "source ./.profile && cd crypto-fantazia/api/ && npm run build && node build/api/src/index.js &"
--- deploy-www.bat
+++ deploy-www.bat
@@ -12,8 +12,8 @@ call ng build --prod
 cd /D "%OLD_DIR%"
 
 :deploy
-pscp -r -i "%~dp0\env\cryptoFanta-heficed_ubuntu.ppk" "%~dp0\www\dist" ubuntu@%HOST%:crypto-fantazia/www/
+pscp -r -i "%~dp0\env\cryptoFanta-heficed_cryptof.ppk" "%~dp0\www\dist" cryptof@%HOST%:crypto-fantazia/www/
 
 :launch
-plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_ubuntu.ppk" ubuntu@%HOST% "killall lite-server"
-plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_ubuntu.ppk" ubuntu@%HOST% "cd crypto-fantazia/www/ && npx lite-server &"
+plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_cryptof.ppk" cryptof@%HOST% "killall lite-server"
+plink -no-antispoof -ssh -i "%~dp0\env\cryptoFanta-heficed_cryptof.ppk" cryptof@%HOST% "cd crypto-fantazia/www/ && npx lite-server &"
